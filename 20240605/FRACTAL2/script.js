1879b0cfb26a2a7246cef414ed9b4ea230f8b14a
Новиков Александр (AN5) <cgsg207@ad.pml30.ru>
2024-06-06 14:24:39 +0300

    Gulia fractal.

--- 20240605/FRACTAL2/script.js
+++ 20240605/FRACTAL2/script.js
@@ -1,17 +1,13 @@
-let
-  canvas,
-  gl,
-  timeLoc;    
- 
-// OpenGL initialization function  
+let canvas, gl, timeLoc;
+
+// OpenGL initialization function
 export function initGL() {
   canvas = document.getElementById("myCan");
   gl = canvas.getContext("webgl2");
-  gl.clearColor(0.30, 0.47, 0.8, 1);
-  
+  gl.clearColor(0.3, 0.47, 0.8, 1);
+
   // Shader creation
-  let vs_txt =
-  `#version 300 es
+  let vs_txt = `#version 300 es
   precision highp float;
   in vec3 InPosition;
     
@@ -21,36 +17,34 @@ export function initGL() {
   void main( void )
   {
     gl_Position = vec4(InPosition, 1);
-    gl_Position.x += 0.1 * sin(Time);
     DrawPos = InPosition.xy;
   }
   `;
-  let fs_txt =
-  `#version 300 es
+  let fs_txt = `#version 300 es
   precision highp float;
   out vec4 OutColor;
   
-  in vec2 DrawPos;
-  uniform float Time;
+    in vec2 DrawPos;
+    uniform float Time;
  
   void main( void )
   {
     vec2 Z = DrawPos.xy;
     vec2 Z0 = Z;
     vec2 C = vec2(0.30 + 0.47 * sin(Time), 0.8);
+    float n = 0.0;
 
-    while (n < 100.0 && lenth(Z) < 2.0)
+    while (n < 100.0 && length(Z) < 2.0)
     {
-      Z = vec2(Z.x * Z.X - Z.Y * Z.Y, 2 * Z.Y * Z.X) + Z0;
-      n+= 1.0;
+      Z = vec2(Z.x * Z.x - Z.y * Z.y, 2.0 * Z.y * Z.x) + C;
+      n++;
     }
-      }
+
     OutColor = vec4(DrawPos, 0.0, 1.0) * 0.5;
     OutColor.g = n / 100.0;
   }
   `;
-  let
-    vs = loadShader(gl.VERTEX_SHADER, vs_txt),
+  let vs = loadShader(gl.VERTEX_SHADER, vs_txt),
     fs = loadShader(gl.FRAGMENT_SHADER, fs_txt),
     prg = gl.createProgram();
   gl.attachShader(prg, vs);
@@ -58,12 +52,25 @@ export function initGL() {
   gl.linkProgram(prg);
   if (!gl.getProgramParameter(prg, gl.LINK_STATUS)) {
     let buf = gl.getProgramInfoLog(prg);
-    console.log('Shader program link fail: ' + buf);
-  }                                            
- 
+    console.log("Shader program link fail: " + buf);
+  }
+
   // Vertex buffer creation
   const size = 0.8;
-  const vertexes = [-size, size, 0, -size, -size, 0, size, size, 0, size, -size, 0];
+  const vertexes = [
+    -size,
+    size,
+    0,
+    -size,
+    -size,
+    0,
+    size,
+    size,
+    0,
+    size,
+    -size,
+    0,
+  ];
   const posLoc = gl.getAttribLocation(prg, "InPosition");
   let vertexArray = gl.createVertexArray();
   gl.bindVertexArray(vertexArray);
@@ -74,13 +81,13 @@ export function initGL() {
     gl.vertexAttribPointer(posLoc, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(posLoc);
   }
- 
+
   // Uniform data
   timeLoc = gl.getUniformLocation(prg, "Time");
- 
+
   gl.useProgram(prg);
-}  // End of 'initGL' function               
- 
+} // End of 'initGL' function
+
 // Load and compile shader function
 function loadShader(shaderType, shaderSource) {
   const shader = gl.createShader(shaderType);
@@ -88,27 +95,28 @@ function loadShader(shaderType, shaderSource) {
   gl.compileShader(shader);
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
     let buf = gl.getShaderInfoLog(shader);
-    console.log('Shader compile fail: ' + buf);
-  }                                            
+    console.log("Shader compile fail: " + buf);
+  }
   return shader;
 } // End of 'loadShader' function
-  
-let x = 1;                    
- 
+
+let x = 1;
+
 // Main render frame function
 export function render() {
   // console.log(`Frame ${x++}`);
   gl.clear(gl.COLOR_BUFFER_BIT);
-                                               
+
   if (timeLoc != -1) {
     const date = new Date();
-    let t = date.getMinutes() * 60 +
-            date.getSeconds() +
-            date.getMilliseconds() / 1000;
- 
+    let t =
+      date.getMinutes() * 60 +
+      date.getSeconds() +
+      date.getMilliseconds() / 1000;
+
     gl.uniform1f(timeLoc, t);
   }
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 } // End of 'render' function
- 
+
 console.log("CGSG forever!!! script.js imported");
