439101776a22c180821a625b261e7ae570254aeb
Новиков Александр (AN5) <cgsg207@ad.pml30.ru>
2024-06-26 18:16:26 +0300

    Project

--- Project/game/game.js
+++ Project/game/game.js
@@ -5,12 +5,56 @@
  */
 
 let DamageShips = [];
+let StepsFind4 = [];
+let StepsFind32 = [];
+
 function CreateDamage() {
   let i, j;
 
   for (i = 0; i < 10; i++) {
     for (j = 0; j < 10; j++) {
       DamageShips[i][j] = false;
+    }
+  }
+}
+
+// Create arrays to Ai step.
+function CreateAIArraysToStep() {
+  let i, j;
+  // look for 4 point ship
+  for (i = 0; i < 10; i++) {
+    if (i % 4 == 2) {
+      StepsFind4.push(vec2(i, 1));
+      StepsFind4.push(vec2(i, 5));
+      StepsFind4.push(vec2(i, 9));
+    } else if (i % 4 == 3) {
+      StepsFind4.push(vec2(i, 0));
+      StepsFind4.push(vec2(i, 4));
+      StepsFind4.push(vec2(i, 8));
+    } else if (i % 4 == 0) {
+      StepsFind4.push(vec2(i, 3));
+      StepsFind4.push(vec2(i, 7));
+    } else if (i % 4 == 1) {
+      StepsFind4.push(vec2(i, 2));
+      StepsFind4.push(vec2(i, 6));
+    }
+  }
+  // look for 2 or 3 point ship
+  for (i = 0; i < 10; i++) {
+    if (i % 4 == 02) {
+      StepsFind32.push(vec2(i, 1));
+      StepsFind32.push(vec2(i, 5));
+      StepsFind32.push(vec2(i, 9));
+    } else if (i % 4 == 1) {
+      StepsFind32.push(vec2(i, 0));
+      StepsFind32.push(vec2(i, 4));
+      StepsFind32.push(vec2(i, 8));
+    } else if (i % 4 == 2) {
+      StepsFind32.push(vec2(i, 3));
+      StepsFind32.push(vec2(i, 7));
+    } else if (i % 4 == 3) {
+      StepsFind32.push(vec2(i, 2));
+      StepsFind32.push(vec2(i, 6));
     }
   }
 }
--- Project/player/PlayerStep.js
+++ Project/player/PlayerStep.js
@@ -1,6 +1,11 @@
 /* Array of player steps */
 let Steps = [[]];
-
+let Damage1 = [];
+let Damage2 = [[]];
+let Damage3 = [[]];
+let Damage4 = [];
+let zone = [];
+let DamageShips = [[]];
 function CreateSteps() {
   let i, j;
 
@@ -8,6 +13,31 @@ function CreateSteps() {
     for (j = 0; j < 10; j++) {
       Step[i][j] = false;
     }
+  }
+}
+
+/* */
+function CreateDamagesArrays() {
+  let i, j;
+
+  for (i = 0; i < 10; i++) {
+    Damage1[i] = Ships1AI[i];
+  }
+
+  for (i = 0; i < 10; i++) {
+    for (j = 0; j < 10; j++) {
+      Damage2[i][j] = Ships2AI[i][j];
+    }
+  }
+
+  for (i = 0; i < 10; i++) {
+    for (j = 0; j < 10; j++) {
+      Damage3[i][j] = Ships3AI[i][j];
+    }
+  }
+
+  for (i = 0; i < 10; i++) {
+    Damage4[i] = Ships4AI[i];
   }
 }
 
@@ -48,13 +78,17 @@ function DelFromArray(sc, pos) {
   if (sc.y == 0) {
     Swap(Ships1AI[sc.x], Ships1[0]);
     Ships1.shift();
-    sinked++;
+
+    sinkedPlayer++;
+    createzone(Damage1[sc.x]);
   }
+
   if (sc.x == 3) {
     Swap(Ship4AI[sc.x], Ship4AI[0]);
     Ship4AI.shift();
     if (Ship4.length == 0) {
-      sinked++;
+      sinkedPlayer++;
+      createzone(Damage4);
     }
   }
   if (sc.x == 1) {
@@ -62,7 +96,10 @@ function DelFromArray(sc, pos) {
     let c = sc.y % 2;
 
     Swap(Ships2AI[num][c], Ships2AI[num][0]);
-    if (Ships2AI[num].lenth == 1) sinked++;
+    if (Ships2AI[num].length == 1) {
+      sinkedPlayer++;
+      createzone(Damage2[num]);
+    }
     Ships2AI[num].shift();
   }
   if (sc.x == 2) {
@@ -70,9 +107,57 @@ function DelFromArray(sc, pos) {
     let c = sc.y % 3;
 
     Swap(Ships3AI[num][c], Ships3AI[num][0]);
-    if (Ships2AI[num].lenth == 1) sinked++;
+    if (Ships3AI[num].lenth == 1) {
+      sinkedPlayer++;
+      createzone(Damage3[num]);
+    }
     Ships3AI[num].shift();
   }
   freePlaceAI[pos.y][pos.x] = true;
   DamageShips[pos.y][pos.x] = true;
+}
+
+function FindInArray(pos, array) {
+  let i, j;
+
+  for (i = 0; i < array.lenth; i++) {
+    for (j = 0; j < array[i].length; i++) {
+      if (array[i][j] == pos) return true;
+    }
+  }
+  return false;
+}
+
+function createzone(array) {
+  let i, j, g;
+  let zonec = [];
+
+  for (i = 0; i < array.length; i++) {
+    zonec = coordzone(array[i]);
+    for (j = 0; j < zonec.length; j++) {
+      for (g = 0; g < array.length; g++) {
+        if (zonec[j] == array[g] || zonec[j].x == 10 || zonec[j].y == 10) {
+          Swap(zonec[j], zonec[0]);
+          zonec.shift();
+        } else {
+          zone.push(zonec[j]);
+        }
+      }
+    }
+  }
+}
+
+function coordzone(pos) {
+  let array = [];
+
+  array[0] = vec2(pos.x - 1, pos.y - 1);
+  array[1] = vec2(pos.x - 1, pos.y);
+  array[2] = vec2(pos.x - 1, pos.y + 1);
+  array[3] = vec2(pos.x, pos.y - 1);
+  array[4] = vec2(pos.x, pos.y + 1);
+  array[5] = vec2(pos.x + 1, pos.y - 1);
+  array[6] = vec2(pos.x + 1, pos.y);
+  array[7] = vec2(pos.x + 1, pos.y + 1);
+
+  return array;
 }
